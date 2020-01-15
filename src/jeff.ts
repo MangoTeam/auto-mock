986f64337d36196058d23ade21aad04aaa7c5301
Dylan Lukes <lukes.dylan@gmail.com>
2020-01-14 16:54:15 -0800

    works now, I think?

--- src/jeff.ts
+++ src/jeff.ts
@@ -1,15 +1,16 @@
 import * as Tree from './Tree';
 
 function main() {
-
     // start the tree-building algorithm from the document's body
     let root = document.body;
     let out = Tree.flatten(Tree.mockify(root));
     Tree.visualize(out);
 
     let result = JSON.stringify(out);
+    (window as any).result = result;
     window.localStorage.clear();
     window.localStorage.setItem('output', result);
 }
 
+console.log("running");
 main();
