16569f2f5cb9d373e913737d75f8db0bef88be44
Dylan Lukes <lukes.dylan@gmail.com>
2020-01-14 16:32:55 -0800

    better jeff mode

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,12 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 
 export default {
-    input: './src/index.ts',
+    input: './src/jeff.ts',
+    output: [
+        {
+            format: 'iife',
+        }
+    ],
     plugins: [
         typescript(),
         nodeResolve(),
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-import * as Tree from './Tree';
-
 import {main} from './NodeBench'
-
-
-// function main() {
-
-//     // start the tree-building algorithm from the document's body
-//     let root = document.body;
-//     let out = Tree.flatten(Tree.mockify(root));
-//     Tree.visualize(out);
-
-//     let result = JSON.stringify(out);
-//     window.localStorage.clear();
-//     window.localStorage.setItem('output', result);
-// }
 
 export default main;
 
