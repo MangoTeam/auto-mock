eee8b11a844e757d04adaaa7d42b10320a5e5d16
Dylan Lukes <lukes.dylan@gmail.com>
2020-01-14 16:24:28 -0800

    forgot the rollup config

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+export default {
+    input: './src/index.ts',
+    plugins: [
+        typescript(),
+        nodeResolve(),
+        commonjs(),
+        json()
+    ]
+}
